5e63a6ee37f96cf90bd6d4a2992815b8e97a01f9
uDaniAlves <daniel.augusto.balves@gmail.com>
2021-03-29 22:49:21 +0100

    Break to sleep

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model, // Cria 
+  },
+
+  seeds(server) {
+    server.db.loadData ({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-01-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-01-15 08:00:00')
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api'; //todas as chamadas À API estao a partir de 'api'
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    }) // quando houver um get para a rota transactions
+
+    this.post('/transactions', (schema, request)=> {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+
+})
 
 ReactDOM.render(
   <React.StrictMode>
